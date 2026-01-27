677351f451be7fc2cf319e12b377a31a5395fcfc
Emdi Gemini <yougotjinx@gmail.com>
2026-01-27 21:40:05 +0800

    added show password for signup page

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axiosInstance from "../../lib/axios";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import toast from "react-hot-toast";
 
 const SignUp = () => {
@@ -8,6 +9,8 @@ const SignUp = () => {
   const [ username, setUsername ] = useState("");
   const [ password, setPassword ] = useState("");
   const [ confirmPassword, setConfirmPassword ] = useState("");
+  const [ showPass, setShowPass ] = useState(false);
+  const [ showPass2, setShowPass2 ] = useState(false);
 
   const signup = async () => {
     console.log("hi");
@@ -47,15 +50,25 @@ const SignUp = () => {
         <input className="border border-dark-gray rounded-lg py-1 px-2 text-txt" type="text" 
         value={username} onChange={(e) => setUsername(e.target.value)} />
       </div>
-      <div className="flex flex-col w-[80%] md:w-100 gap-1">
+      <div className="relative flex flex-col w-[80%] md:w-100 gap-1">
         <label className="text-txt" htmlFor="password">Create Password:</label>
-        <input className="border border-dark-gray rounded-lg py-1 px-2 text-txt" type="password" 
+        <input className="border border-dark-gray rounded-lg py-1 px-2 text-txt" type={showPass ? "text" : "password"} 
         value={password} onChange={(e) => setPassword(e.target.value)} />
+        {showPass 
+        ? <FaEye className="absolute bottom-0.5 right-1.5 -translate-x-1/2 -translate-y-1/2 cursor-pointer text-amber-50" 
+        onClick={() => setShowPass(false)}/>
+        : <FaEyeSlash className="absolute bottom-0.5 right-1.5 -translate-x-1/2 -translate-y-1/2 cursor-pointer text-amber-50"
+        onClick={() => setShowPass(true)} />}
       </div>
-      <div className="flex flex-col w-[80%] md:w-100 gap-1">
+      <div className="relative flex flex-col w-[80%] md:w-100 gap-1">
         <label className="text-txt" htmlFor="password">Confirm Password:</label>
-        <input className="border border-dark-gray rounded-lg py-1 px-2 text-txt" type="password" 
+        <input className="border border-dark-gray rounded-lg py-1 px-2 text-txt" type={showPass2 ? "text" : "password"} 
         value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+        {showPass2 
+        ? <FaEye className="absolute bottom-0.5 right-1.5 -translate-x-1/2 -translate-y-1/2 cursor-pointer text-amber-50" 
+        onClick={() => setShowPass2(false)}/>
+        : <FaEyeSlash className="absolute bottom-0.5 right-1.5 -translate-x-1/2 -translate-y-1/2 cursor-pointer text-amber-50"
+        onClick={() => setShowPass2(true)} />}
       </div>
       <button className="text-white font-bold tracking-wider w-[80%] md:w-100 bg-blue p-3 rounded-xl cursor-pointer disabled:cursor-wait disabled:opacity-70" 
       onClick={signup} disabled={loading} >SIGN UP</button>
